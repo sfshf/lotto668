493b8980e19f1a66b9ef2d293610b5cc215f6148
sfshf <sfshf@github.com>
2024-04-19 15:03:36 +0800

    hh test --network localhost

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -6,16 +6,26 @@ module.exports = {
   paths: {
     artifacts: "./src/artifacts",
   },
+  defaultNetwork: "hardhat",
   networks: {
-    fuji: {
-      url: process.env.QUICKNODE_URL,
-      accounts: [`0x` + process.env.PRIVATE_KEY],
-      chainId: 43113,
+    localhost: {
+      url: "http://127.0.0.1:8545"
     },
-    avax: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
-      accounts: [`0x` + process.env.PRIVATE_KEY],
-      chainId: 43114,
+    hardhat: {
     },
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY]
+    // },
+    // fuji: {
+    //   url: process.env.QUICKNODE_URL,
+    //   accounts: [`0x` + process.env.PRIVATE_KEY],
+    //   chainId: 43113,
+    // },
+    // avax: {
+    //   url: "https://api.avax.network/ext/bc/C/rpc",
+    //   accounts: [`0x` + process.env.PRIVATE_KEY],
+    //   chainId: 43114,
+    // },
   },
 };
